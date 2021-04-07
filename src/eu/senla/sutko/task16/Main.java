d2e30bd3e39d8b862c9212e63df114897a89bcd7
SUTIK <SUTIK90@MAIL.RU>
2021-04-07 16:45:41 +0300

    удаление пока не работает

--- src/eu/senla/sutko/task16/Main.java
+++ src/eu/senla/sutko/task16/Main.java
@@ -3,18 +3,18 @@ package eu.senla.sutko.task16;
 import eu.senla.sutko.task16.orders.Order;
 import eu.senla.sutko.task16.products.Product;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    static int idProd ;
+
     public static void main(String[] args) {
+
         try {
             while (true){
                 Scanner sc = new Scanner(System.in);
@@ -31,16 +31,27 @@ public class Main {
                         System.out.println("4 - Выход");
                         int i1 = sc.nextInt();
                         if (i1==1){
-                            System.out.println(readProduct());
-                        }else if (i1==2){
+                            //показываем список
+
+                            for (Product p:readProduct()
+                                 ) {
+                                System.out.println(p);
+                            }
+                        }
+                        else if (i1==2){
                             //добавляем продукт
                             addProduct();
                         }
-                         else if (i1 != 3) {
-                            if (i1 == 4) {
-                                break;
-                            } else System.out.println("Неверная комманда");
+                         else if (i1 == 3) {
+                            //удаляем продукт
+                            deleteProduct();
+
                         }
+                         else if (i1 == 4) {
+                            break;
+                        }
+                         else { System.out.println("Неверная комманда");}
+
 
                     }
                 }else if (i==2){
@@ -52,7 +63,7 @@ public class Main {
                         System.out.println("4 - Выход");
                         int i1 = sc.nextInt();
                         if (i1==1){
-                            System.out.println(readOrder());
+                           // System.out.println(readOrder());
                         }else if (i1==2){}
                         else if (i1==3){}
                         else if(i1==4){break; }
@@ -70,64 +81,95 @@ public class Main {
 
 
 
+    private static void deleteProduct(){
+        List<Product>pr=new ArrayList<>(readProduct());
+        System.out.println("Введите id продукта");
+        Scanner scanner = new Scanner(System.in);
+        int i1 = scanner.nextInt();
+        Iterator<Product> iterator = pr.iterator();
+        while(iterator.hasNext()) {
+            Product nextProd = iterator.next();//получаем следующий элемент
+            if (nextProd.getDate().equals(i1)) {
+                iterator.remove();
+            }
+        }
+
+
+      // for (Product i:readProduct()) {
+      //     if (i.getId()==i1){
+      //         pr.remove(i);
+      //     }
+      // }
+
+            for(Product r:pr)
+            {
+                try { BufferedWriter out = new BufferedWriter(new FileWriter("C:\\Users\\Sergey\\IdeaProjects\\HomeWork16\\src\\eu\\senla\\sutko\\task16\\product.txt",true));
+                    String sum=String.valueOf(r.getId())+"/"+r.getName()+"/"+String.valueOf(r.getDate());
+                    out.write(sum+"\r\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+    }
+
 
 
 
 
     private static List<Product> readProduct() {
         List<Product> productList = new ArrayList<>();
-        //читаем из файла и создаем обьекты Товар, которые запихиваем в наш productList
         try {
             File file = new File("C:\\Users\\Sergey\\IdeaProjects\\HomeWork16\\src\\eu\\senla\\sutko\\task16\\product.txt");
-            Scanner scan = new Scanner(file);
-            while (scan.hasNextLine()) {
-                String line = scan.nextLine();
-                String[] s = line.split("/");
-                Product prod1 = new Product(Integer.parseInt(s[0]), s[1], getDate(s[2]));
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                String line = sc.nextLine();
+                String[]s = line.split("/");
+                Product prod1 = new Product(Integer.parseInt(s[0]),s[1],getDate(s[2]) );
                 productList.add(prod1);
-                return productList;
-            }
+        }
 
-        } catch (Exception ex) { ex.getMessage(); }
-
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return productList;
     }
 
 
-
-    private static List<Order> readOrder(){
+    private static List<Order> readOrer() {
         List<Order> orderList = new ArrayList<>();
-        try{
+        try {
             File file = new File("C:\\Users\\Sergey\\IdeaProjects\\HomeWork16\\src\\eu\\senla\\sutko\\task16\\order.txt");
-            Scanner scan = new Scanner(file);
-            while (scan.hasNextLine()){
-                String line = scan.nextLine();
-                String[] s = line.split("/");
-                Order order1 =new Order(Integer.parseInt(s[0]),getProductList(s[1]),getDate(s[2]));
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                String line = sc.nextLine();
+                String[]s = line.split("/");
+                Order order1 = new Order(Integer.parseInt(s[0]),getProductList(s[1]),getDate(s[2]));
                 orderList.add(order1);
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        }
-        catch (Exception ex){ ex.getMessage();
-        }
         return orderList;
     }
 
 
+    //сплит пробуктов в заказе
     private static String[] getProductList (String s1){
         String[] splitProd = s1.split(",");
         return splitProd;
     }
 
-
+    //сплит даты
     private static LocalDate getDate(String s) {
         String[] split = s.split("-");
         return  LocalDate.of(Integer.parseInt(split[2]) ,Integer.parseInt(split[1]),Integer.parseInt(split[0]));
     }
 
     private static void addProduct(){
-        List<Product> l = new ArrayList<>(readProduct());
-        for (Product i:l) {
+        int idProd=0 ;
+        for (Product i:readProduct()) {
              idProd = i.getId();
         }
        int id= idProd+1;
@@ -136,10 +178,10 @@ public class Main {
         String nameProd = scanner.nextLine();
         System.out.println("Введите дату в фотрмате 21-03-2021");
         String dateProd=scanner.nextLine();
-        String newProduct = "\n"+id+"/"+nameProd+"/"+dateProd;
+        String newProduct = id+"/"+nameProd+"/"+dateProd;
         try{
             try (BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\Sergey\\IdeaProjects\\HomeWork16\\src\\eu\\senla\\sutko\\task16\\product.txt", true))) {
-                bw.write(newProduct);
+            bw.write(newProduct+"\r\n");
             }
             System.out.println("Добавлено "+newProduct);
 
